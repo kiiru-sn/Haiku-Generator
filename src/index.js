84bb0acbf295abb14da3baefe826b4f09212dfdf
kiiru-sn <kiiru.sn@gmail.com>
2023-12-29 14:32:16 +0000

    added form handling and added typewritter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateHaiku(event) {
+  event.preventDefault();
 
+  new Typewriter("#haiku", {
+    strings: "Generating haiku...",
+    autoStart: true,
+    loop: "false",
+  });
+}
+
+let haikuFormElement = document.querySelector("#haiku-generator-form");
+haikuFormElement.addEventListener("submit", generateHaiku);
